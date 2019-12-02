a63bdce0a77c050edb5b810487f6465249781728
Mustafa Anas <khmostafa4@gmail.com>
2019-12-01 20:26:27 -0500

    add the new template

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-diamond"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Hunna</h1>
+                <p> <strong>Mothers of the revolution, </strong></p><p>A documentary that tells the story of the Syrian revolution</p>
+                <p>as seen by the eyes of its mothers</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
